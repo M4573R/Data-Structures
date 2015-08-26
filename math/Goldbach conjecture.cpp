b34d4348042c906f5a340932a4f450d9d6297fc6
M4573R <tasmiah.tahsin@hotmail.com>
2015-08-26 10:20:54 -0700

    Create Goldbach conjecture.cpp

---
+++ math/Goldbach conjecture.cpp
@@ -0,0 +1,5 @@
+http://www.algorithmist.com/index.php/Prime_Sieve_of_Eratosthenes.cpp
+
+http://www.algolist.net/Algorithms/Number_theoretic/Sieve_of_Eratosthenes
+
+http://www.algorithmist.com/index.php/UVa_543
